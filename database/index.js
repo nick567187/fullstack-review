eee1011b90d01b56f4940a6eb20d3777afbc1db7
Hack Reactor Student <student@hackreactor.com>
2018-09-01 11:25:48 -0700

    asdf

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB|| 'mongodb://localhost/fetcher');
+mongoose.connect( process.env.MONGODB || 'mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   name: String,

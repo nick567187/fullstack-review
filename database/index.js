1ba4f8a76fce92f7c42ffb7395bb03561c493cb2
Hack Reactor Student <student@hackreactor.com>
2018-09-01 12:29:37 -0700

    asdf

--- database/index.js
+++ database/index.js
@@ -10,13 +10,12 @@ let repoSchema = mongoose.Schema({
 let Repo = mongoose.model('Repo', repoSchema);
 
 let save = (data) => {
-
   data.forEach(function(elem) {
    console.log(elem.login)
     var item = new Repo ({
-       name: elem.name,
-       htmlurl: elem['html_url'],
-       login: elem.owner.login
+      name: elem.name,
+      htmlurl: elem['html_url'],
+      login: elem.owner.login
     });
     item.save(function(err) {
       if(err) { console.log(err) };
